97461c6c7e0b948e55e1ebc8af25db85e278e0cd
yu88569 <chiangta.jirayu@gmail.com>
2024-11-13 00:34:50 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-500 text-5xl'>sasukrongkwang</h1>
+      <h1 className='text-blue-500 text-5xl'>sasukrongkwang</h1>
       <HomePage />
     </div>
   );
